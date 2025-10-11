03a8a87c568eaaa9fd62150060d2fcec76c00b11
ice4ed <18493861+ice4ed@users.noreply.github.com>
2025-10-11 03:52:37 -0500

    Update hornys_gabz.lua
    Added category

--- server/unusedLocations.lua/hornys_gabz.lua
+++ server/unusedLocations.lua/hornys_gabz.lua
@@ -2,7 +2,7 @@ Jobs['hornys'] = {
     CateringEnabled = true,
     closedShopsEnabled = true,
     Blip = {
-        {sprite = 80, color = 2, scale = 0.5, label = 'hornys', loc = vector4(1251.36, -353.5, 74.02, 96.8)},
+        {sprite = 80, color = 2, scale = 0.5, label = 'hornys', loc = vector4(1251.36, -353.5, 74.02, 96.8), category = 15 },
     },
     closedShops = {
         {ped = 'mp_m_freemode_01', loc = vec4(1239.19, -367.96, 69.21, 217.86), label = 'Horny Little Shop'}
@@ -134,4 +134,5 @@ Jobs['hornys'] = {
         sirloin_burger = {anim = 'eat', label = 'Eating', add = {hunger = 10}},
         wings = {anim = 'eat', label = 'Eating', add = {hunger = 10}},
     },
+
 }
