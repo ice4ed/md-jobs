a56464348f4b7d7025e7c6755490c819f65b6249
ice4ed <18493861+ice4ed@users.noreply.github.com>
2025-10-11 04:06:08 -0500

    Update Deerdiner_dippzy.lua
    formatting

--- server/unusedLocations.lua/Deerdiner_dippzy.lua
+++ server/unusedLocations.lua/Deerdiner_dippzy.lua
@@ -2,7 +2,7 @@ Jobs['deerdiner'] = {
     CateringEnabled = true,
     closedShopsEnabled = true,
     Blip = {
-        {sprite = 469, color = 2, scale = 0.8, label = 'Deer Diner', loc = vector3(454.46, 131.0, 99.4), category = 15 },
+        {sprite = 469, color = 2, scale = 0.8, label = 'Deer Diner', loc = vector3(454.46, 131.0, 99.4), category = 15},
     },
     closedShops = {
         {ped = 'mp_m_freemode_01', loc = vec4(454.52, 131.05, 99.4, 151.87), label = 'Deer Diner Shop'}
@@ -142,3 +142,4 @@ Jobs['deerdiner'] = {
     },
 
 }
+
